45be1fdb145ab627cd7d9dad73c51aaabcc52c62
doti <saminassaminas@gmail.com>
2023-08-23 00:48:02 +0100

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import {RouterModule, Routes} from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContentWrapperComponent } from './content-wrapper/content-wrapper.component';
+import { ControlSidebarComponent } from './control-sidebar/control-sidebar.component';
+import { MainFooterComponent } from './main-footer/main-footer.component';
+import { MainHeaderComponent } from './main-header/main-header.component';
+import { MainSidebarComponent } from './main-sidebar/main-sidebar.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'dashboard', component: ContentWrapperComponent,
+    // children: [
+    //   {path: 'home', component: MainFooterComponent}
+    // ]
+  },
+
+]
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContentWrapperComponent,
+    ControlSidebarComponent,
+    MainFooterComponent,
+    MainHeaderComponent,
+    MainSidebarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
